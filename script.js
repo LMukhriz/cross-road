a76f06aec55dd4a991fa19ae3b2828ca09144ff4
LMukhriz <idzuanmukhriz0@gmail.com>
2024-09-02 15:47:54 +0800

    Update script.js

--- script.js
+++ script.js
@@ -16,9 +16,9 @@ camera.position.y = initialCameraPositionY;
 camera.position.x = initialCameraPositionX;
 camera.position.z = distance;
 
-const zoom = 5;
+const zoom = 1;
 
-const chickenSize = 199;
+const chickenSize = 150;
 
 const positionWidth = 42;
 const columns = 17;
